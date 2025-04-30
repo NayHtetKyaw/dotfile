c99695c8b0d6d6b75aafc684f3183c78e8bf8aed
NayHtetKyaw <nayhtetkyaw.dev@gmail.com>
2025-04-30 15:03:12 +0700

    new noise config and fixes conform

--- nvim/.config/nvim/lua/plugins/noise.lua
+++ nvim/.config/nvim/lua/plugins/noise.lua
@@ -1,20 +1,63 @@
 return {
 	"folke/noice.nvim",
-	event = "VeryLazy",
+
+	event = { "BufReadPost", "BufWritePost", "BufNewFile", "CmdlineEnter" },
+	-- event = { 'CmdlineEnter' },
 	opts = {
-		presets = {
-			bottom_search = false, -- use a classic bottom cmdline for search
-			command_palette = true, -- position the cmdline and popupmenu together
-			long_message_to_split = true, -- long messages will be sent to a split
-			inc_rename = false, -- enables an input dialog for inc-rename.nvim
-			lsp_doc_border = false, -- add a border to hover docs and signature help
-		},
+		-- add any options here
 	},
 	dependencies = {
+		-- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
 		"MunifTanjim/nui.nvim",
 		-- OPTIONAL:
 		--   `nvim-notify` is only needed, if you want to use the notification view.
 		--   If not available, we use `mini` as the fallback
 		"rcarriga/nvim-notify",
 	},
+	config = function()
+		require("noice").setup({
+			routes = {
+				{
+					view = "notify",
+					filter = { event = "msg_showmode" },
+				},
+			},
+			messages = {
+				-- NOTE: If you enable messages, then the cmdline is enabled automatically.
+				-- This is a current Neovim limitation.
+				enabled = false, -- enables the Noice messages UI
+				-- view = "notify", -- default view for messages
+				-- view_error = "notify", -- view for errors
+				-- view_warn = "notify", -- view for warnings
+				-- view_history = "messages", -- view for :messages
+				view_search = "virtualtext", -- view for search count messages. Set to `false` to disable
+			},
+			popupmenu = { enabled = false },
+			notify = { enabled = false },
+
+			-- you can enable a preset for easier configuration
+			presets = {
+				-- bottom_search = true, -- use a classic bottom cmdline for search
+				bottom_search = false, -- use a classic bottom cmdline for search
+				-- command_palette = true, -- position the cmdline and popupmenu together
+				command_palette = true, -- position the cmdline and popupmenu together
+				-- long_message_to_split = true, -- long messages will be sent to a split
+				inc_rename = true, -- enables an input dialog for inc-rename.nvim
+				lsp_doc_border = true, -- add a border to hover docs and signature help
+				-- lsp_doc_border = true, -- add a border to hover docs and signature help
+			},
+		})
+		-- vim.keymap.set('n', '<leader>nd', '<cmd>Noice dismiss<cr>')
+		-- vim.keymap.set("n", "<leader>nc", "<cmd>Noice disable<cr>")
+		-- vim.keymap.set("n", "<leader>ne", "<cmd>Noice enable<cr>")
+		vim.keymap.set("n", "<leader>nt", "<cmd>Noice telescope<cr>")
+	end,
 }
+-- :Noice or :Noice history shows the message history
+-- :Noice last shows the last message in a popup
+-- :Noice dismiss dismiss all visible messages
+-- :Noice errors shows the error messages in a split. Last errors on top
+-- :Noice disable disables Noice
+-- :Noice enable enables Noice
+-- :Noice stats shows debugging stats
+-- :Noice telescope opens message history in Telescope
--- zsh/.zshrc
+++ zsh/.zshrc
@@ -113,8 +113,10 @@ export PATH=~/.npm-global/bin:$PATH
 ## SHELL INTEGRATIONS ##
 #######################$
 
-eval "$(fzf --zsh)"
-eval "$(zoxide init --cmd cd zsh)"
+# eval "$(fzf --completion zsh)"
+[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
+# eval "$(zoxide init --cmd cd zsh)"
+eval "$(zoxide init zsh)"
 
 ###########
 ## ALIAS ##
@@ -153,3 +155,5 @@ fpath=($HOME/completion_zsh $fpath)
 fpath=(~/.docker/completions \/home/basu/.local/share/zinit/completions /usr/local/share/zsh/site-functions /usr/share/zsh/site-functions /usr/share/zsh/functions/Calendar /usr/share/zsh/functions/Chpwd /usr/share/zsh/functions/Completion /usr/share/zsh/functions/Completion/Base /usr/share/zsh/functions/Completion/Linux /usr/share/zsh/functions/Completion/Unix /usr/share/zsh/functions/Completion/X /usr/share/zsh/functions/Completion/Zsh /usr/share/zsh/functions/Exceptions /usr/share/zsh/functions/MIME /usr/share/zsh/functions/Math /usr/share/zsh/functions/Misc /usr/share/zsh/functions/Newuser /usr/share/zsh/functions/Prompts /usr/share/zsh/functions/TCP /usr/share/zsh/functions/VCS_Info /usr/share/zsh/functions/VCS_Info/Backends /usr/share/zsh/functions/Zftp /usr/share/zsh/functions/Zle /home/basu/.local/share/zinit/plugins/zsh-users---zsh-completions/src /home/basu/.local/share/zinit/plugins/Aloxaf---fzf-tab/lib)
 autoload -Uz compinit
 compinit
+
+alias bat="batcat"
